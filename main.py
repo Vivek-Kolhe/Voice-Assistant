2008d259aaa733258716e1ea27d990d5dba1d06b
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2020-09-03 18:36:35 +0530

    Added Wikipedia

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from gtts import gTTS
 import os
 import playsound
 import random
+import wikipedia
 
 def hearing():
     r = sr.Recognizer()
@@ -28,6 +29,25 @@ def speak(voice_data):
 
 def main():
     speak("Hey, what's up?")                        # greeting
-    pass
+    voice_data = hearing()
+
+    if "search wikipedia for" in voice_data:
+        voice_data = voice_data.split()
+        results = wikipedia.search(voice_data[3:])
+        speak("Here are the results I found.")
+        for i in range(len(results)):
+            print(f"{i+1}. {results[i]}")
+        speak("What do you want to search in particular? Enter index of the result")
+        choice = int(input("Enter index: "))
+        print(wikipedia.summary(results[choice-1]) + "\n----")
+        page_url = wikipedia.page(results[choice-1]).url
+        print(f"Page url: {page_url}")
+        speak("Would you like me to open the page?")
+        choice_2 = input("Enter (y/n): ")
+        if choice_2 == "y":
+            pass
+        else:
+            pass
+
 
 main()
