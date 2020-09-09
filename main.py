a7701a3294723457014152680637922c79d9c9fa
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2020-09-09 17:12:41 +0530

    Added game

--- main.py
+++ main.py
@@ -30,55 +30,91 @@ def speak(voice_data):
     playsound.playsound(audio_file)                 # playing audio file
     os.remove(audio_file)                           # deleting audio file
 
+def respond(voice_data):
+    speak(voice_data)
+    print(voice_data)
+
 def main():
-    speak("Hey, what's up?")                        # greeting
+    respond("Hey, what's up?")                        # greeting
     voice_data = hearing()
 
     if voice_data == "who are you" or voice_data == "what is your name" or voice_data == "what's your name":
-        speak("I'm  Athena.")                       # gives introduction
+        respond("I'm  Athena.")                       # gives introduction
 
     if voice_data == "what time is it" or voice_data == "what is the time" or voice_data == "tell me the time":
         local_time = time.asctime()
         local_time = local_time[11:19]
         time_words = f"{local_time[:2]} hours and {local_time[3:5]} minutes and {local_time[6:]} seconds."
-        speak(time_words)                           # tells you time
+        respond(time_words)                           # tells you time
 
     if "search google for" in voice_data:           # google search
         query = voice_data.split()
         query = query[3:]
-        speak("Opening browser.")
+        respond("Opening browser.")
         webbrowser.open("https://www.google.com/search?q=" + "%20".join(query))
 
     if "search wikipedia for" in voice_data:        # searches wikipedia
         voice_data = voice_data.split()
         results = wikipedia.search(voice_data[3:])
-        speak(f"Here is what I found for {voice_data[3:]}.")
+        respond(f"Here is what I found for {voice_data[3:]}.")
         for i in range(len(results)):
             print(f"{i+1}. {results[i]}")
-        speak("What do you want to search in particular? Enter index of the result")
+        respond("What do you want to search in particular? Enter index of the result")
         choice = int(input("Enter index: "))
         print(wikipedia.summary(results[choice-1]) + "\n----")
         page_url = wikipedia.page(results[choice-1]).url
         print(f"Page url: {page_url}")
-        speak("Would you like me to open the page?")
+        respond("Would you like me to open the page?")
         choice_2 = input("Enter (y/n): ")
         if choice_2 == "y":
             webbrowser.open(page_url)
         else:
-            speak("Ok!")
+            respond("Ok!")
+    
+    if voice_data == "rock paper scissors":             # plays rock paper scissors
+        speak("How many rounds you want to play?")
+        rounds = int(input("How many rounds you want to play? "))
+        computer, user = 0, 0
+        for i in range(rounds):
+            comp_turn = random.choice(["rock", "paper", "scissors"])
+            print("Your turn!")
+            user_turn = hearing()
+
+            if comp_turn == "rock" and user_turn == "scissors":
+                computer += 1
+            elif comp_turn == "paper" and user_turn == "rock":
+                computer += 1
+            elif comp_turn == "scissors" and user_turn == "paper":
+                computer += 1
+            elif user_turn == "rock" and comp_turn == "scissors":
+                user += 1
+            elif user_turn == "paper" and comp_turn == "rock":
+                user += 1
+            elif user_turn == "scissors" and comp_turn == "paper":
+                user += 1
+            else:
+                computer += 0
+                user += 0
+        
+        if computer == user:
+            respond("Draw!")
+        elif computer > user:
+            respond("You lose!")
+        else:
+            respond("You won!")
     
     if voice_data == "chuck norris":                # gets random chuck norris joke
         response = requests.get("https://api.chucknorris.io/jokes/random")
         data = response.json()
-        speak(data["value"])
+        respond(data["value"])
     
     if voice_data == "advice" or voice_data == "give me an advice":
         response = requests.get("https://api.adviceslip.com/advice")
         data = response.json()
-        speak(data["slip"]["advice"])               # gives random advice
+        respond(data["slip"]["advice"])               # gives random advice
     
     if voice_data == "goodbye" or voice_data == "bye" or voice_data == "quit":
-        speak("See ya later!")                      # quits the program
+        respond("See ya later!")                      # quits the program
         exit()
 
 main()
